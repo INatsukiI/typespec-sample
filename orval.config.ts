ebe2c4d4e9c24e93bb926d599b4ade139bef43d2
INatsukiI <e1q20019@oit.ac.jp>
2025-05-18 23:14:38 +0900

    fix: build→generatedに変更

--- orval.config.ts
+++ orval.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'orval';
 export default defineConfig({
   backend: {
     input: {
-      target: './build/openapi/api.yaml',
+      target: './generated/openapi/api.yaml',
     },
      output: {
-       target: './build/frontend/api.ts',
+       target: './generated/frontend/api.ts',
        client: "react-query",
      },
    },
